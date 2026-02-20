5140ce98cc21e825e835a425061f81be82d041e5
Nathanial Henniges <19924836+nathanialhenniges@users.noreply.github.com>
2026-02-20 07:35:10 -0600

    Add Facebook-style console warning to deter social engineering

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("Loading script.js");
+
+(function () {
+    console.log(
+        "%cStop!",
+        "color: #e74c3c; font-size: 48px; font-weight: bold;"
+    );
+    console.log(
+        "%cThis browser feature is intended for developers. If someone told you to copy-paste something here to enable a feature or \u2018hack\u2019 an account, it is a scam and will give them access to your account.",
+        "font-size: 16px;"
+    );
+})();
